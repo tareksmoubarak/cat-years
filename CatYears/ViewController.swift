21cacd7fea92f8e8b4c7be0321ec52cbe674c0a6
Tarek Moubarak <tarek@hellotree.co>
2018-09-23 00:17:45 +0300

    App Done

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var catAge: UITextField!
+    @IBOutlet weak var ageInCatYears: UILabel!
+    
+    @IBAction func calculateAge(_ sender: Any) {
+        let _catAgeVal = Int(catAge.text!)
+        let _ageInCatAge = _catAgeVal! * 7
+        ageInCatYears.text = "Your cat is " + String(_ageInCatAge)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
 
 
